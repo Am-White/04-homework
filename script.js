dac7f17ac052508dd7daccd5a1ffa531331257d3
littlefire <aybwhite@gmail.com>
2020-10-23 20:22:35 -0700

    First commit- variables and prompts asking what characters they would like to include

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 //Arrays: Variables
 var passwordLength = " ";
-var addLowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var addUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var addLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var addUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var addSymbols = ["!@#$%^&*()_+={}|][:;'?/.>,<`~"];
 var addNumbers = ["1234567890"];
 
@@ -16,8 +16,20 @@ function writePassword() {
     if (passwordLength <= 6 || passwordLength >= 100) {
       alert ("Password must be between 8 and 100 characters!!"); return;
     }
+    //Confirming password length
+    alert ("Your password will be " + passwordLength + " characters");
 
+//Confirming arrays: Variables
+var confirmAddLowercase = confirm("If you would like to include lowercase characters? = click OKAY");
+var confirmAddUppercase = confirm("If you would like to include uppercase characters? = click OKAY");
+var confirmAddSymbols = confirm("If you would like to include symbol characters? = click OKAY");
+var confirmAddNumbers = confirm("If you would like to include numbers? = click OKAY");
 
+var passwordParameter = [];
+
+if (confirmAddLowercase) {
+  passwordParameter.push(addLowercase);
+}
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
